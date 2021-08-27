9045f9689e5d6a1e21195c7a11b5d05097eff9a0
plumwiserim <dulfla_5@naver.com>
2021-08-27 17:05:52 +0900

    practice

---
+++ practice/practice/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by SW_mac04 on 2021/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // 뷰가 로드 되었다.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+        // 뷰가 나타날 것이다.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+        // 뷰가 나타났다.
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        <#code#>
+        // 뷰가 사라질 것이다.
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        <#code#>
+        // 뷰가 사라졌다.
+    }
+
+    
+}
+
