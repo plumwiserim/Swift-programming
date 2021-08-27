95577a2f676500a27a8c8263893bf2cbbdb7387f
plumwiserim <dulfla_5@naver.com>
2021-08-27 17:01:25 +0900

    projectK

---
+++ 210827/K/projectK/SecondVC.swift
@@ -0,0 +1,21 @@
+//
+//  SecondVC.swift
+//  projectK
+//
+//  Created by SW_mac04 on 2021/08/27.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        self.view.backgroundColor = .blue
+    }
+
+    
+
+}
+
